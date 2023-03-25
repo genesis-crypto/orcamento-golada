8c086d3f94cd23b3295c6a2c35f3d053e8079ae9
Pedro <p-cardozo@hotmail.com>
2023-03-25 16:40:35 -0300

    refactor(orcamento): change concrete implentation from Calculadora

--- calculadora/calculadora.go
+++ calculadora/calculadora.go
@@ -1,7 +1,9 @@
 package calculadora
 
+import "orcamento-golada/orcamento"
+
 type Calculadora struct{}
 
-func (c *Calculadora) RealizaCalculo(orcamento float64) float64 {
-	return orcamento
+func (c *Calculadora) RealizaCalculo(orcamento *orcamento.Orcamento) float64 {
+	return orcamento.GetValorOrcamento()
 }
