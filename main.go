e15aa4d0d277c3adc7fed1692aed1211aee4437e
Pedro <p-cardozo@hotmail.com>
2023-03-26 16:32:22 -0300

    refactor(main): include on main state for orcamento & update on variables names

--- main.go
+++ main.go
@@ -2,36 +2,43 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"orcamento-golada/calculadora"
 	"orcamento-golada/desconto"
 	"orcamento-golada/orcamento"
 )
 
 func main() {
-	calculadoraConcreta := &calculadora.Calculadora{}
+	calculadoraBase := &calculadora.Calculadora{}
 
 	calculadoraICMS := &calculadora.CalculadoraICMS{
-		Calculadora: calculadoraConcreta,
+		Calculadora: calculadoraBase,
 	}
 
 	calculadoraPIS := &calculadora.CalculadoraPIS{
 		Calculadora: calculadoraICMS,
 	}
 
-	produtos := []orcamento.Produto{
+	orcamentoPrincipal := orcamento.CriaOrcamento([]orcamento.Produto{
 		{Nome: "Notebook", Valor: 8000, Quantia: 10},
-	}
+	})
 
-	orcamento := &orcamento.Orcamento{
-		Produtos: produtos,
-	}
-
-	fmt.Println(">> must be", calculadoraPIS.RealizaCalculo(orcamento))
+	fmt.Println(">> must be", calculadoraPIS.RealizaCalculo(orcamentoPrincipal))
 
 	descontoValor := &desconto.DescontoPorValor{}
 
 	descontoQuantia := &desconto.DescontoPorQuantia{}
 	descontoQuantia.SetNext(descontoValor)
 
-	descontoQuantia.Executa(orcamento)
+	descontoQuantia.Executa(orcamentoPrincipal)
+
+	orcamentoPrincipal.AdicionaDescontoExtra()
+
+	if err := orcamentoPrincipal.Aprova(); err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	if err := orcamentoPrincipal.Finaliza(); err != nil {
+		log.Fatalf(err.Error())
+	}
 }
